628a804304a83a5c31bc66e8d9a6841c606c9f3b
apagidiyala <apagidiyala@hawk.iit.edu>
2026-07-07 20:33:57 -0400

    Added company logo

--- script.js
+++ script.js
@@ -14,6 +14,15 @@ document.querySelectorAll("form").forEach((form) => {
   form.addEventListener("submit", function (event) {
     event.preventDefault();
 
+    // Generate Reservation ID
+const reservationId =
+  "2WS-" +
+  new Date().getFullYear() +
+  String(new Date().getMonth() + 1).padStart(2, "0") +
+  String(new Date().getDate()).padStart(2, "0") +
+  "-" +
+  Date.now().toString().slice(-6);
+
     const note = form.querySelector(".form-note");
 
     if (note) {
@@ -21,11 +30,26 @@ document.querySelectorAll("form").forEach((form) => {
     }
 
     emailjs
-      .sendForm("service_gipisdi", "template_v9i5zrl", this)
-      .then(() => {
-  form.reset();
-  window.location.href = "payment.html";
-})
+  .sendForm("service_gipisdi", "template_v9i5zrl", this)
+
+  .then(() => {
+
+    // Send confirmation email to customer
+    return emailjs.sendForm(
+      "service_gipisdi",
+      "template_vjyeisg",
+      form
+    );
+
+  })
+
+  .then(() => {
+
+    form.reset();
+
+    window.location.href = "payment.html";
+
+  })
       .catch((error) => {
         console.error("EmailJS error:", error);
 
