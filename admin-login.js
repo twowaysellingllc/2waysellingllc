7d7844dc93363bd095695451decd31f12fba9fa1
apagidiyala <apagidiyala@hawk.iit.edu>
2026-07-12 16:29:55 -0400

    Show reservation details on success page

--- admin-login.js
+++ admin-login.js
@@ -15,16 +15,17 @@ form.addEventListener("submit", async (e) => {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    const { error } = await supabase.auth.signInWithPassword({
-        email,
-        password
-    });
+   const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+});
 
-    if(error){
+console.log("DATA:", data);
+console.log("ERROR:", error);
 
+    if (error) {
         document.getElementById("error").innerHTML = error.message;
         return;
-
     }
 
     window.location.href = "admin.html";
