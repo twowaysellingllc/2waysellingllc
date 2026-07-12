5597de6799c71a5b66617f45487cebf864106c7a
apagidiyala <apagidiyala@hawk.iit.edu>
2026-07-12 17:20:19 -0400

    Fix admin login

--- admin-login.js
+++ admin-login.js
@@ -1,33 +1,32 @@
-const SUPABASE_URL = "https://ymcpxaheusrxgmgitceu.supabase.co";
-const SUPABASE_KEY = "sb_publishable_WYKUu85jk5xwMutqo6Ex3Q_q6PvtOK4";
+if (!window.adminLoaded) {
+    window.adminLoaded = true;
 
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
+    const SUPABASE_URL = "https://ymcpxaheusrxgmgitceu.supabase.co";
+    const SUPABASE_KEY = "sb_publishable_WYKUu85jk5xwMutqo6Ex3Q_q6PvtOK4";
 
-const form = document.getElementById("loginForm");
+    const supabase = window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
 
-form.addEventListener("submit", async (e) => {
+    const form = document.getElementById("loginForm");
 
-    e.preventDefault();
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
 
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
 
-   const { data, error } = await supabase.auth.signInWithPassword({
-    email,
-    password
-});
+        const { error } = await supabase.auth.signInWithPassword({
+            email,
+            password
+        });
 
-console.log("DATA:", data);
-console.log("ERROR:", error);
+        if (error) {
+            document.getElementById("error").textContent = error.message;
+            return;
+        }
 
-    if (error) {
-        document.getElementById("error").innerHTML = error.message;
-        return;
-    }
-
-    window.location.href = "admin.html";
-
-});
+        window.location.href = "admin.html";
+    });
+}
