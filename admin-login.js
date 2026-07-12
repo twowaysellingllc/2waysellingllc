5a70eac73437d5053c38f13e94fb3e0eb1ee8d3e
apagidiyala <apagidiyala@hawk.iit.edu>
2026-07-12 13:46:06 -0400

    Added admin login

---
+++ admin-login.js
@@ -0,0 +1,32 @@
+const SUPABASE_URL = "https://ymcpxaheusrxgmgitceu.supabase.co";
+const SUPABASE_KEY = "sb_publishable_WYKUu85jk5xwMutqo6Ex3Q_q6PvtOK4";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password
+    });
+
+    if(error){
+
+        document.getElementById("error").innerHTML = error.message;
+        return;
+
+    }
+
+    window.location.href = "admin.html";
+
+});
